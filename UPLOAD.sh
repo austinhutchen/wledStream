e26d42d805981f09843692db6c074a60e84dfc1d
Austin Hutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-28 07:35:46 -0800

    Update UPLOAD.sh

--- UPLOAD.sh
+++ UPLOAD.sh
@@ -1,4 +1,4 @@
-esptool.exe -p COM10 erase_flash esptool.exe -p COM10 write_flash Ox0 C3_bootloader.bin
+esptool.exe -p COM10 erase_flash esptool.exe -p COM10 write_flash 0x0 C3_bootloader.bin
 esptool.exe -p COM10 write_flash 0x8000 С3_partitions.bin
 esptool.exe -p COM10 write_flash 0x010000
 6_may_lolin_c3_mini_T2.bin
